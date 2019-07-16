ed6a272bf3b32345e7c2d8af1b9eeae5233a2cee
Lucas Lopes <lucasclopesr@gmail.com>
2019-07-15 21:39:40 -0300

    Implement simple channel publishing sensor for testing

---
+++ src/sensor.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+  ch := make(chan string)
+
+  go getReadings(ch);
+}
+
+func getReadings(ch chan<- string) {
+  // generate readings
+  start := time.Now()
+
+  r_temperature := rand.Float64()
+  r_humidity := rand.Float64()
+  r_luminosity := rand.Float64()
+
+  ch <- fmt.Sprint("%.2fs:\n\ttemperature: %f\n\thumidity: %f\n\tluminosity: %f", start, r_temperature, r_humidity, r_luminosity)
+}
